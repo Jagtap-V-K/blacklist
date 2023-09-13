425b5610be7718e5bdddfec6691d759b0973b99e
JagtapVK <vyankateshkiranjagtap33@gmail.com>
2023-09-13 21:49:47 +0530

    12th commit

--- PatternFn.java
+++ PatternFn.java
@@ -1,22 +1,75 @@
-public class PatternFn { 
-    public static void HollowRectangle(int totRows, int totCols) { 
-        //outer loop
-        for(int i=1; i<=totRows; i++) { 
-            //inner columns 
-            for(int j=1; j<=totCols; j++) { 
-                //cell- (1,1) 
-                if(i == 1 || 1 == totRows || j == 1 || j == totCols) { 
-                    //boundary cells                    
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
-    
+import java.util.*;
+ public class PatternFn { 
+    // public static void HollowRectangle(int totRows, int totCols) { 
+        
+    //     for( int i = 1; i <= totRows; i++ ) { 
+            
+    //         for( int j = 1; j <= totCols; j++ ) { 
+                
+    //             if( i == 1 || i == totRows || j == 1 || j == totCols ) { 
+                                     
+    //                 System.out.print("*");
+    //             } else {
+    //                 System.out.print(" ");
+    //             }
+    //         }
+    //         System.out.println();  
+    //     }   
+    // }
+    // public static void Pyramid(int line) { 
+        
+    //     for( int i = 1; i <= line; i++ ) { 
+            
+    //         for( int j = 1; j <= line - i; j++ ) { 
+    //                 System.out.print(" ");
+    //             }
+    //         for( int j = 1; j <= i; j++ ) { 
+    //                 System.out.print("*");
+    //             }
+    //             System.out.println();  
+    //         }
+    //     }
+
     public static void main(String args[]) {
-        HollowRectangle(5, 5);
+        // HollowRectangle(5, 5);
+        // Pyramid(5);
+        // for(int i = 1; i<=5; i++){
+        //     for(int j = 1; j<=5-i+1; j++){
+        //         System.out.print(j);
+        //     }
+        //         System.out.println();
+        // }
+        // Floyd();
+
+    }            
+
+    // public static void Floyd(){
+    //     int n=1;
+    //     for(int i = 1; i<=5; i++){                      //1
+    //         for(int j = 1; j<=i; j++){                  //2     3
+    //             System.out.print(n +"\t");              //4     5       6
+    //             n++;                                    //7     8       9       10
+    //         }                                           //11    12      13      14
+    //             System.out.println();
+    //     }
+
+    //     //inverted
+    //     for(int i = 1; i<=5; i++){                      //15
+    //         for(int j = 5; j>=i; j--){                  //16    17
+    //             System.out.print(n +"\t");              //18    19      20
+    //             n++;                                    //21    22      23      24
+    //         }
+    //             System.out.println();
+    //     }
+
+        //
+        // for(int i = 1; i<=5; i++){                      
+        //     for(int j = 5; j>=i; j--){                  
+        //         System.out.print(n +"\t");              
+        //         n++;                                    
+        //     }
+        //         System.out.println();
+        // }
+
     }
 }
