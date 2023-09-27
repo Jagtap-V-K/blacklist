3152e9087a1997bab5e4518ba187d5e8eeab441e
JagtapVK <vyankateshkiranjagtap33@gmail.com>
2023-09-27 23:21:23 +0530

    20

--- Sorting.java
+++ Sorting.java
@@ -3,9 +3,14 @@
 //Insertion sort       basic
 //Counting sort     advance
 
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Sorting{
     public static void main(String args[]){
     int a[] = {5, 4, 1, 3, 2};
+    Integer b[] = {6, 9, 7, 8, 5, 4, 1, 3, 2};
     
     //Bubble - Large element come to the end of
     //array by swapping with adjacent element
@@ -42,7 +47,7 @@ public class Sorting{
 
     //Incertion- Pick an element from unsorted part
     //& place in the right position in sorted part
-    for(int i=1; i<a.length; i++){
+    /*for(int i=1; i<a.length; i++){
         int CurrNo = a[i];
         int PrevNo = i-1;
         while(PrevNo >= 0 && a[PrevNo] > CurrNo){
@@ -53,14 +58,24 @@ public class Sorting{
     }
     for(int i=0; i<a.length; i++){
         System.out.print(a[i]+" ");
+    }*/
+
+    //Inbuilt - only ascending
+    //TC = O(n*logn)
+    /*Arrays.sort(b);
+    // Arrays.sort(b, 0, 4);   // will sort upto 4th index i.e. 0 1 2 3
+    for(int i=0; i<b.length; i++){
+        System.out.print(b[i]+" ");
+    }*/
+
+    //for reverse
+    //as off reverse order function only works on objects
+    //so we need to conver int(primtive) to Integer(Object)
+    // Arrays.sort(b, Collections.reverseOrder());
+    Arrays.sort(b, 2, 7, Collections.reverseOrder());
+    for(int i=0; i<b.length; i++){
+        System.out.print(b[i]+" ");
     }
-
-
-
-
-
-
-
 
 
 
