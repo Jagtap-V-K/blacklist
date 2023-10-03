b6b813660846f71e8601da0f3e12a5ead1ec246d
JagtapVK <vyankateshkiranjagtap33@gmail.com>
2023-10-04 00:02:56 +0530

    23

--- D2Ary.java
+++ D2Ary.java
@@ -22,41 +22,45 @@ public class D2Ary{
 
 
         //Spiral Matrix
-        int a = { 
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12},
-                {13,14,15,16}
-        };
+        int matrix[][] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
 
         int startRow = 0;
-        int endRow = a.length - 1;
         int startCol = 0;
-        int endCol = a[0].length - 1;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
 
-        while(startRow <= endRow && startCol <=endCol){
+        //print boundaries
+        while(startRow <= endCol && startCol <= endCol) {
+            for(int j=startCol; j<=endCol; j++) {
+                System.out.print(matrix[startRow][j]+" ");
+            }
             
-            for(int j =startCol; j<=endCol; j++ ){
-                system.out.print(a[startRow][j] +" ");
-            }
-            for(int i =startRow+1; i<=endRow; i++ ){
-                system.out.print(a[i][endCol] +" ");
-            }
-            for(int j =endCol -1; j<=startCol; j-- ){
-                system.out.print(a[endRow][j] +" ");
-            }
-            for(int i =endRow-1; j<=startRow+1; j--){
-                system.out.print(a[i][startCol] +" ");
+            for(int i=startRow+1; i<=endRow; i++) {
+                System.out.print(matrix[i][endCol]+" ");
             }
 
-            startCol++;
-            startRow++;
-            endCol--;
-            endRow--;
+            for(int j=endCol-1; j>=startCol; j--) {
+                if(startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j]+" ");
+            }
 
+            for(int i=endRow-1; i>startRow; i--) {
+                if(startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol]+" ");
+            }
+
+            startCol++; startRow++;
+            endCol--; endRow--;
         }
 
-        
+        System.out.println();
 
         
     }
