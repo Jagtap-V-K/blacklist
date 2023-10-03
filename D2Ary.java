4bae52163194ac550f27ebca02d60be76521ed54
JagtapVK <vyankateshkiranjagtap33@gmail.com>
2023-10-03 23:57:27 +0530

    22

--- D2Ary.java
+++ D2Ary.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class D2Ary{
     public static void main(String args[]){
 
-        int a[][] = new int[3][4];
+        /*int a[][] = new int[3][4];
         int n = a.length;       //3
         int m = a[0].length;    //4
         Scanner sc = new Scanner(System.in);
@@ -17,7 +17,47 @@ public class D2Ary{
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
+        }*/
+
+
+
+        //Spiral Matrix
+        int a = { 
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        int startRow = 0;
+        int endRow = a.length - 1;
+        int startCol = 0;
+        int endCol = a[0].length - 1;
+
+        while(startRow <= endRow && startCol <=e ndCol){
+            
+            for(int j =startCol; j<=endCol; j++ ){
+                system.out.print(a[startRow][j] +" ");
+            }
+            for(int i =startRow+1; i<=endRow; i++ ){
+                system.out.print(a[i][endCol] +" ");
+            }
+            for(int j =endCol -1; j<=startCol; j-- ){
+                system.out.print(a[endRow][j] +" ");
+            }
+            for(int i =endRow-1; j<=startRow+1; j--){
+                system.out.print(a[i][startCol] +" ");
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+
         }
+
+        
+
         
     }
 }
